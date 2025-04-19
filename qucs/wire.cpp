Radvall/qucs_s

commit 20f83ba786b9115e446b6617c5c02fbbbb9cf0e7

refactor: Add and use another Wire ctor

qucs/wire.cpp
@@ -21,7 +21,7 @@
 
 #include <QPainter>
 
-Wire::Wire(int _x1, int _y1, int _x2, int _y2, Node *n1, Node *n2)
+Wire::Wire(int _x1, int _y1, int _x2, int _y2)
 {
   x1 = _x1;
   y1 = _y1;
@@ -32,12 +32,18 @@ Wire::Wire(int _x1, int _y1, int _x2, int _y2, Node *n1, Node *n2)
   cx = (x1 + x2) / 2;
   cy = (y1 + y2) / 2;
 
-  Port1 = n1;
-  Port2 = n2;
+  Port1 = nullptr;
+  Port2 = nullptr;
   Label = nullptr;
 
   Type = isWire;
   isSelected = false;
+}
+
+Wire::Wire(Node* n1, Node* n2)
+{
+  connectPort1(n1);
+  connectPort2(n2);
 }
 
 Wire::~Wire()

qucs/schematic_element.cpp
@@ -540,14 +540,9 @@ Wire* Schematic::selectedWire(int x, int y)
 // Splits the wire "*pw" into two pieces by the node "*pn".
 Wire* Schematic::splitWire(Wire *source_wire, Node *splitter_node)
 {
-    Wire *new_wire = new Wire(splitter_node->cx, splitter_node->cy, source_wire->x2, source_wire->y2, splitter_node, source_wire->Port2);
+    Wire *new_wire = new Wire(splitter_node, source_wire->Port2);
     new_wire->isSelected = source_wire->isSelected;
-
     source_wire->connectPort2(splitter_node);
-
-    new_wire->Port2->connect(new_wire);
-    splitter_node->connect(new_wire);
-    new_wire->Port2->disconnect(source_wire);
     a_Wires->push_back(new_wire);
 
     if(source_wire->Label)
@@ -2374,12 +2369,7 @@ std::pair<bool,Node*> Schematic::installWire(Wire* wire)
         } else {
             // … the given wire has been already used to fill another gap.
             // Fill this gap with a brand new wire.
-	        auto* w = new Wire(node_pair.first->x(), node_pair.first->y(),
-			                   node_pair.second->x(), node_pair.second->y());
-            w->Port1 = node_pair.first;
-            w->Port1->connect(w);
-            w->Port2 = node_pair.second;
-            w->Port2->connect(w);
+	        auto* w = new Wire(node_pair.first, node_pair.second);
             a_Wires->push_back(w);
             has_changes = true;
         }
@@ -2793,15 +2783,9 @@ void Schematic::dumbConnectWithWire(const QPoint& a, const QPoint& b) noexcept {
         auto m = points[i-1];
         auto n = points[i];
 
-        auto* wire = new Wire(m.x(), m.y(), n.x(), n.y());
+        auto* wire = new Wire(new Node(m.x(), m.y()), new Node(n.x(), n.y()));
         a_Wires->push_back(wire);
-
-        wire->Port1 = new Node(m.x(), m.y());
-        wire->Port1->connect(wire);
         a_Nodes->push_back(wire->Port1);
-
-        wire->Port2 = new Node(n.x(), n.y());
-        wire->Port2->connect(wire);
         a_Nodes->push_back(wire->Port2);
     }
 }

qucs/schematic_file.cpp
@@ -909,7 +909,7 @@ bool Schematic::loadWires(QTextStream *stream, std::list<Element*> *List)
     Line = Line.trimmed();
     if(Line.isEmpty()) continue;
 
-    w = new Wire(0,0,0,0, nullptr, nullptr);
+    w = new Wire();
     if(!w->load(Line)) {
       QMessageBox::critical(0, QObject::tr("Error"),
       QObject::tr("Format Error:\nWrong 'wire' line format!"));

qucs/wire.h
@@ -28,7 +28,8 @@ class QString;
 
 class Wire : public Conductor {
 public:
-  Wire(int _x1=0, int _y1=0, int _x2=0, int _y2=0, Node *n1=0, Node *n2=0);
+  Wire(int _x1=0, int _y1=0, int _x2=0, int _y2=0);
+  Wire(Node* n1, Node* n2);
  ~Wire() override;
 
   void paint(QPainter* painter) const;

commit 8261edaa813d6d6f808098434a9acd4adf9c4dfc

fix: Avoid crash when moving a wire or a component
This fixes regression introduced in commit 20f83ba7 (ra3xdh#1315).
New Wire constructor added in the named commit doesn't properly
initialize all member fields which may lead to crashes. It seems
that only GCC builds are affected.

qucs/wire.cpp
@@ -39,6 +39,7 @@ Wire::Wire(int _x1, int _y1, int _x2, int _y2)
 }
 
 Wire::Wire(Node* n1, Node* n2)
+  : Wire(n1->x(), n1->y(), n2->x(), n2->y())
 {
   connectPort1(n1);
   connectPort2(n2);